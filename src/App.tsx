b9f15df76d080b6b9c0f12b65c7bb3974d1d9738
Niaz <faridani.rad@gmail.com>
2023-11-13 16:11:31 +0100

    Add vite react ts visx mantine zustand react query template

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import {
+  Title,
+  Text,
+  Button,
+  Card,
+  Center,
+  MantineProvider,
+  Stack,
+} from "@mantine/core";
+import Graph from "./Graph";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <div
+        style={{
+          paddingTop: 150,
+          position: "fixed",
+          left: 0,
+          top: 0,
+          width: "100%",
+          height: "100%",
+          overflowY: "auto",
+          backgroundSize: "cover",
+        }}
+      >
+        <Stack align="center" justify="flex-end">
+          <Graph width={1000} height={300} />
+
+          <Center>
+            <Card>
+              <Title
+                variant="gradient"
+                gradient={{ from: "#43cea2", to: "#185a9d" }}
+                weight={700}
+                align="center"
+              >
+                Hackthon Frontend
+              </Title>
+              <Button
+                variant="light"
+                color="blue"
+                fullWidth
+                mt="md"
+                radius="md"
+                onClick={() => setCount((count) => count + 1)}
+              >
+                count is {count}
+              </Button>
+              <Text>
+                Edit <code>src/App.tsx</code> and save to test HMR
+              </Text>
+            </Card>
+          </Center>
+        </Stack>
+      </div>
+    </MantineProvider>
+  );
+}
+
+export default App;
