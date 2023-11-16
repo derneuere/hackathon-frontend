4224088884d181d1a5e41631e08befc35f2b6506
Niaz <faridani.rad@gmail.com>
2023-11-16 15:19:41 +0100

    Add nice looking subsciption card

--- src/SubscribeCard.tsx
+++ src/SubscribeCard.tsx
@@ -1,18 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Card, Flex, Stack, Text, Title, Progress } from "@mantine/core";
+import { Card, TextInput, Stack, Text, Title, ActionIcon } from "@mantine/core";
 import { useGraphDataStore, useStatisticsStore } from "./Store";
+import { IconSend } from "@tabler/icons-react";
+import { notifications } from "@mantine/notifications";
+import {
+  useRegistriereMitIndex,
+  RegistriereMitIndexVariables,
+} from "./client/datenbrauereiComponents";
+import { mockdata } from "./Helper";
+import { AbfrageDimension } from "./client/datenbrauereiSchemas";
 
 export function SubscribeCard() {
-  const { circles } = useStatisticsStore((state) => state);
+  const { variables } = useStatisticsStore((state) => state);
   const { graphData } = useGraphDataStore((state) => state);
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+
+  const registriereMitIndexMutation = useRegistriereMitIndex();
   //test
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
       <Stack>
         <Stack>
-          <Title order={3}>Kreis:</Title>
-          <Flex gap="md" direction={{ base: "column", sm: "row" }}></Flex>
+          <Title order={3}>
+            Werde informiert, wenn sich dein Index ändert!
+          </Title>
+          <TextInput
+            label={<Text c="dimmed">Gebe deinen Index einen Namen!</Text>}
+            value={name}
+            onChange={(event) => setName(event.currentTarget.value)}
+          ></TextInput>
+          <TextInput
+            label={
+              <Text c="dimmed">
+                Wohin sollen wir Deine Benachrichtigung senden?
+              </Text>
+            }
+            value={email}
+            onChange={(event) => setEmail(event.currentTarget.value)}
+            placeholder="Deine E-Mail Adresse"
+            rightSection={
+              <ActionIcon>
+                <IconSend
+                  onClick={() => {
+                    const dimensionen: AbfrageDimension[] = variables.map(
+                      (item) => {
+                        let weight: "NULL" | "EINS" | "ZWEI" | "DREI" = "NULL";
+                        if (item.weight == 1) {
+                          weight = "EINS";
+                        }
+                        if (item.weight == 2) {
+                          weight = "ZWEI";
+                        }
+                        if (item.weight == 3) {
+                          weight = "DREI";
+                        }
+
+                        return {
+                          merkmalName:
+                            mockdata.find((m) => m.title === item.name)
+                              ?.querykey || "",
+                          gewichtung: weight,
+                        };
+                      }
+                    );
+                    const formData: RegistriereMitIndexVariables = {
+                      body: {
+                        email: email,
+                        name: name,
+                        abfragen: [
+                          {
+                            dimensionen: dimensionen,
+                          },
+                        ],
+                      },
+                    };
+                    registriereMitIndexMutation.mutate(formData);
+                    notifications.show({
+                      title: "Index aboniert",
+                      message:
+                        "Wir informieren dich sobald dein Index sich ändert!",
+                      color: "green",
+                    });
+                    setEmail("");
+                    setName("");
+                  }}
+                />
+              </ActionIcon>
+            }
+          ></TextInput>
         </Stack>
       </Stack>
     </Card>
